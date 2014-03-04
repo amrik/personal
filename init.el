0e1d44a15f5bda6c0bc8471f84ff8e8ff9762812
Amrik Kochhar <amrik@squareup.com>
2014-03-04 00:05:54 -0800

    Initial commit

---
+++ init.el
@@ -0,0 +1,10 @@
+    (setq backup-directory-alist
+          `((".*" . ,temporary-file-directory)))
+    (setq auto-save-file-name-transforms
+          `((".*" ,temporary-file-directory t)))
+(prelude-require-packages '(ag color-theme-sanityinc-tomorrow))
+(guru-mode -1)
+(color-theme-sanityinc-tomorrow-bright)
+(setq prelude-guru nil)
+(custom-set-variables '(coffee-tab-width 2))
+(custom-set-variables '(tags-case-fold-search nil))
